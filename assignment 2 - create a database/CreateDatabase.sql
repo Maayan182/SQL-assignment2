68105cad7418e788533c0ee58f69cd173673c31b
Maayan182 <mbtpem@gmail.com>
2024-07-11 00:18:44 +0300

    Add files via upload

---
+++ assignment 2 - create a database/CreateDatabase.sql
@@ -0,0 +1,102 @@
+﻿USE master
+CREATE DATABASE Maayan
+go
+USE Maayan
+go
+
+CREATE TABLE Publishers
+(
+	PublisherID int primary key identity(1,1),
+	PublisherName nvarchar(30) not null,
+	PublisherCountry nvarchar(30) not null
+)
+
+CREATE TABLE Books
+(
+	ISBN varchar(13) primary key,
+	Title nvarchar(100) not null,
+	BookDes text,
+	PublisherID int foreign key references Publishers(PublisherID),
+	Category nvarchar(30) default 'unclassified' not null,
+	BookLanguage nvarchar(30)
+)
+
+CREATE TABLE Authors
+(
+	AuthorID int Primary key identity(1,1),
+	AuthorName nvarchar(30) not null,
+	AuthorBirthDate date,
+	AuthorCountry nvarchar(30) not null,
+)
+
+CREATE TABLE Book_Authors
+(
+	ISBN varchar(13) foreign key references Books(ISBN),
+	AuthorID int foreign key references Authors(AuthorID)
+	constraint PK_BooksAuthors primary key (ISBN,AuthorID)
+
+)
+
+CREATE TABLE Members
+(
+	Email varchar(320) primary key,
+	FirstName nvarchar(30) not null,
+	LastName nvarchar(30) not null,
+	NumOfBooksAllowed int default 1 not null,
+	MonthlySubscription money not null default 50
+
+)
+
+
+CREATE TABLE Items
+(
+	ItemID int primary key identity(1,1),
+	ISBN varchar(13) foreign key references Books(ISBN),
+	SavedFor varchar(320) foreign key references Members(Email) default null,
+	BookStatus bit default 1
+)
+
+ 
+CREATE TABLE Borrowings
+(
+	
+	ItemID int foreign key references Items(ItemID) not null,
+	MemberEmail varchar(320) foreign key references Members(Email) not null,
+	BorrowingDate date not null default convert(date,getdate()),
+	DueDate date not null default Dateadd(month,2,convert(date,getdate())),
+	ReturnedDate date,
+	Fine money,
+	FinePaid bit default 1
+)
+
+-- שלא יהיה אפשר שאותו פריט יושאל באותו טווח זמנים פעמיים וגם שלא יהיה אפשר להשאיל פריט שלא החזירו עדיין
+--סופר כמה פעמים הפריט מופיע בטבלה בטווח תאריכים שמתנגש עם הטווח שהכנסתי וכמה הוא מופיע עם תאריך השאלה של הזמנה פתוחה שהוא לפני התאריך החזרה שהכנסתי 
+go
+create function CheckRange(@Item int,@BorrowingDate date, @ReturnedDate date)
+	returns int
+	begin
+
+	declare @Count int
+
+	select @Count = COUNT(*)
+	from Borrowings
+	where ItemID = @Item AND
+	(@BorrowingDate < ReturnedDate AND @ReturnedDate > BorrowingDate)  or
+	(ReturnedDate is null AND ItemID = @Item AND @ReturnedDate > BorrowingDate)
+		
+	return @Count
+
+end
+go
+-- פריט יכול להופיע בטבלת השאלות רק פעם אחת עם התנאים הנ"ל או לא בכלל
+ALTER TABLE Borrowings
+ADD constraint Returned CHECK(dbo.CheckRange(ItemID,BorrowingDate,ReturnedDate) <= 1)
+
+
+
+
+
+
+
+
+
---
+++ assignment 2 - create a database/PopulateDatabase.sql
@@ -0,0 +1,77 @@
+﻿USE Maayan
+go
+
+INSERT INTO Publishers
+SELECT distinct Publication,PublisherCountry
+FROM  librarything_maayan
+where Publication is not null
+
+INSERT INTO Books
+select ISBN,Title,Summary,P.PublisherID,COALESCE(Subjects,'unclassified'),Languages
+from librarything_maayan L left join Publishers P
+	on L.Publication = P.PublisherName
+
+UPDATE Books
+set Title= N'אומנות ההקשבה לפעימות הלב'
+where ISBN = '9655450864'
+
+INSERT INTO Authors (AuthorName,AuthorCountry,AuthorBirthDate)
+select distinct Primary_Author,AuthorCountry,AuthorBirthdate
+from librarything_maayan
+
+
+INSERT INTO Book_Authors
+select ISBN,AuthorID
+from librarything_maayan join Authors
+	 on AuthorName = Primary_Author
+
+INSERT INTO Members (Email,FirstName,LastName)
+values('Maayan@gmail.com','Maayan','BT'),('Mor@gmail.com','Mor','Ganon'),('Gil@gmail.com','Gil','H'),('Dor@gmail.com','Dor','Ami')
+
+INSERT INTO Members 
+values('Moria@gmail.com','Moria','Even',3,100)
+
+select * from Items
+
+INSERT INTO Items
+select ISBN,null,1
+from Books
+
+INSERT INTO Items
+select ISBN,null,1
+from Books
+where ISBN like '%x%' or ISBN like '%14%'
+
+update Items
+set SavedFor = 'Maayan@gmail.com'
+where ItemID = 18
+
+INSERT INTO Borrowings (ItemID,MemberEmail,BorrowingDate,DueDate)
+values(1,'Mor@gmail.com','2024-02-25','2024-04-25'),
+(2,'Dor@gmail.com',CONVERT(date,'25-02-2024',105),'2024-04-25'),
+(3,'Gil@gmail.com',CONVERT(date,'25-02-2024',105),'2024-04-25'),
+(4,'Moria@gmail.com',CONVERT(date,'31-12-2023',105),'2024-02-28')
+
+UPDATE Items
+SET BookStatus = 0
+where ItemID in(1,2,3,4)
+
+INSERT INTO Borrowings (ItemID,MemberEmail,BorrowingDate,ReturnedDate,DueDate)
+values(2,'Mor@gmail.com',CONVERT(date,'20-04-2023',105),CONVERT(date,'25-05-2023',105),'2023-05-25'),
+(1,'Mor@gmail.com',CONVERT(date,'26-05-2023',105),CONVERT(date,'29-05-2023',105),'2023-05-29'),
+(6,'Dor@gmail.com',CONVERT(date,'20-01-2024',105),CONVERT(date,'20-02-2024',105),'2024-02-20'),
+(9,'Gil@gmail.com',CONVERT(date,'23-01-2024',105),CONVERT(date,'20-02-2024',105),'2024-03-29'),
+(2,'Maayan@gmail.com',CONVERT(date,'20-01-2023',105),CONVERT(date,'20-02-2023',105),'2024-03-25')
+
+INSERT INTO Borrowings (ItemID,MemberEmail,BorrowingDate,Fine,FinePaid,DueDate)
+values(12,'Maayan@gmail.com',CONVERT(date,'20-12-2023',105),50,0,'2024-02-25'),
+(10,'Dor@gmail.com',CONVERT(date,'29-12-2023',105),50,0,'2024-02-25')
+
+UPDATE Items
+SET BookStatus = 0
+where ItemID in(12,10)
+
+INSERT INTO Borrowings (ItemID,MemberEmail,BorrowingDate,ReturnedDate,Fine,FinePaid,DueDate)
+values(5,'Mor@gmail.com',CONVERT(date,'20-04-2022',105),CONVERT(date,'25-07-2022',105),50,1,'2022-06-25'),
+(6,'Gil@gmail.com',CONVERT(date,'20-04-2023',105),CONVERT(date,'25-07-2023',105),50,1,'2023-06-25'),
+(2,'Gil@gmail.com',CONVERT(date,'23-07-2023',105),CONVERT(date,'25-12-2023',105),70,0,'2023-09-23')
